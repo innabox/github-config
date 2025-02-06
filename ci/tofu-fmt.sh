c03d77617490e7f6ffe6f059e2deedbdcbbd1d3a
Lars Kellogg-Stedman <lars@redhat.com>
2025-02-06 11:25:48 -0500

    An intentional error

--- ci/tofu-fmt.sh
+++ ci/tofu-fmt.sh
@@ -1,6 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 
-if ! tofu fmt -check -diff "$@"; then
-  tofu fmt "$@"
+set -x
+
+tofu fmt -no-color -check -diff "$@" > tofu.out 2>&1
+res=$?
+echo "RES: $res"
+
+cat tofu.out
+
+if grep -qi error tofu.out && [[ $res -eq 0 ]]; then
+  echo SOMETHING IS WRONG
   exit 1
 fi
+
+exit $res
