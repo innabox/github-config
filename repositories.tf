981dec5df43b6e270afe66618bbb2c534c0339a4
Lars Kellogg-Stedman <lars@redhat.com>
2025-03-14 11:40:44 -0400

    Forked repositories do not use template repository

--- repositories.tf
+++ repositories.tf
@@ -105,10 +105,11 @@ module "repo_fulfillment_api" {
 }
 
 module "repo_hypershift_cluster_config" {
-  source      = "./modules/common_repository"
-  visibility  = "public"
-  name        = "hypershift-cluster-config"
-  description = "Configuration for OpenShift cluster running ACM w/ Hosted Control Planes"
+  source              = "./modules/common_repository"
+  visibility          = "public"
+  name                = "hypershift-cluster-config"
+  use_public_template = false
+  description         = "Configuration for OpenShift cluster running ACM w/ Hosted Control Planes"
   teams = [
     {
       team_id    = "fulfillment-wg"
